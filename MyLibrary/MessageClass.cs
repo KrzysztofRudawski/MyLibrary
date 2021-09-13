573683c8641ac693c18a433d9e6af255c82b84f9
Krzysztof Rudawski <krzysztof.rudawski@km.kongsberg.com>
2021-09-13 14:22:52 +0200

    Initial commit

---
+++ MyLibrary/MessageClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class MessageClass
+    {
+        public static string GetMessage()
+        {
+            return $"{DateTime.Now} This is test message";
+        }
+    }
+}
